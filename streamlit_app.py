e295365ddaa4884548da31a15a11c36dcff19d23
EmyB44 <emilie.brosseau@gmail.com>
2023-10-15 22:13:16 +0200

    graphique supplémentaire

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,21 @@ import streamlit as st
 import requests
 import pandas as pd
 import plotly.graph_objects as go
+import streamlit as st
+import shap
 
 
 df = pd.read_csv('X_train.csv')
 
-# Titre information sur les clients
-st.title("Informations sur les clients")
 
 # Sélectionnez un numéro de client à l'aide d'un widget de sélection
 selected_client = st.selectbox("Sélectionnez un numéro de client :", df['SK_ID_CURR'])
+
+
+# Titre information sur les clients
+st.title("Informations sur les clients")
+
+
 
 if st.button("Prédire"):
     if selected_client:
@@ -37,13 +43,10 @@ if st.button("Prédire"):
 st.title("Prédictions remboursements sur les clients")
 
 
-
-selected_client = st.selectbox("Sélectionnez un numéro de client :", df['SK_ID_CURR'])
-
 if st.button("Prédire"):
     if selected_client:
         # Effectuer une requête à l'API Flask pour obtenir les prédictions de probabilité
-        response = requests.get(f"https://api-projet-7-emilie-brosseau.onrender.com/predict_proba/{selected_client}")
+        response = requests.get(f"https://api-projet-7-emilie-brosseau.onrender.com/predict_proba/130972")
 
         if response.status_code == 200:
             predictions_proba = response.json()
@@ -77,6 +80,23 @@ if st.button("Prédire"):
 
 
 
+#Features les plus importantes
+st.title("Features importances sur les clients")
 
 
+if st.button("Afficher le graphique SHAP"):
+    if selected_client:
+        # Effectuer une requête à l'API pour obtenir les valeurs SHAP
+        response = requests.get(f"https://api-projet-7-emilie-brosseau.onrender.com/client-info/130972")
 
+        if response.status_code == 200:
+            # Récupérer le graphique SHAP au format JSON depuis l'API
+            shap_chart_json = response.json()
+
+            # Afficher le graphique dans Streamlit en utilisant st.pyplot
+            st.pyplot(generate_shap_chart(shap_chart_json))
+
+        else:
+            st.error("Erreur lors de la récupération du graphique SHAP depuis l'API.")
+    else:
+        st.warning("Sélectionnez un client avant d'afficher le graphique SHAP.")
